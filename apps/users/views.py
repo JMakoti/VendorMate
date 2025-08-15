427791ae378b009df8bbd2b8dc2e659ed29fa925
Brayan <brayanmwangi732@gmail.com>
2025-08-15 14:55:21 +0300

    user model

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+from .models import *
+
 
 # Create your views here.
--- vendormate/settings.py
+++ vendormate/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'users',
+    'rest_framework',
 
+]
+AUTH_USER_MODEL = 'users.manageUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
