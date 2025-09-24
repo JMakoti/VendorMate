bb8decfbe31a05020fa0408b1d1b04faa97ecb5e
JMakoti <josephmwamuye11@gmail.com>
2025-09-24 15:01:39 +0300

    adjust users  routes and document the app

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gender = models.CharField(max_length=255,blank=True, null=True)
+    gender = models.CharField(max_length=255, blank=True, null=True)
     company_name = models.CharField(max_length=255, blank=True, null=True)
     company_address = models.CharField(max_length=255, blank=True, null=True)
     
     def __str__(self):
-        return self.company_name
+        return self.company_name or f"Profile for {self.user.username}"
     
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -9,10 +9,5 @@ urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('login/', login_user, name='login_user'),
     path('users/', get_all_users, name='get_all_users'),
-    path('editprofile/', AddprofileDetails, name='edit_profile'),
-    path('update_profile/', update_profile, name='update_profile'),
     path('change_credentials/', change_user_credentials, name='change_user_credentials'),
-    # path('manage_user/<int:user_id>/', manage_user_details, name='manage_user_details'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
